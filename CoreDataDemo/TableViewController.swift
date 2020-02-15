f6f86de6d3c43087c1d6bd2f17c49afdb58b0fd1
Artur <artur.anissimov@gmail.com>
2020-02-15 04:48:46 +0200

    Display CoreData data Finishing to-do list app

--- CoreDataDemo/TableViewController.swift
+++ CoreDataDemo/TableViewController.swift
@@ -53,6 +53,20 @@ class TableViewController: UITableViewController {
         
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        
+        do {
+            toDoItems = try context.fetch(fetchRequest)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
